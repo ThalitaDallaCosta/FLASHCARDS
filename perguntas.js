b2e6f9ed44db52f6a8eb7424c04ccac7e66f2249
ThalitaDallaCosta <164032463+ThalitaDallaCosta@users.noreply.github.com>
2024-10-16 09:33:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Português',
+    'De quem é a famosa frase "penso, logo existo"?',
+    'Descartes'
+)
+
+criaCartao(
+    'História',
+    'qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'João Gulart'
+)
+
+criaCartao(
+    'Matemática',
+    'Quantas casas decimais tem o numero pi?',
+    'Infinitas'
+)
+
+criaCartao(
+    'Química',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    '118'
+)
